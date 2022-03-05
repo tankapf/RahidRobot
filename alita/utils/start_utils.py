18c14c8b06bbffccec9163c71ff2a8f5cf3165a1
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-03-05 16:04:33 +0400

    update start_utils.py

--- alita/utils/start_utils.py
+++ alita/utils/start_utils.py
@@ -75,7 +75,7 @@ async def gen_start_kb(q: Message or CallbackQuery):
                 (f"🌐 {(tlang(q, 'start.language_btn'))}", "chlang"),
                 (
                     f"🗃️ {(tlang(q, 'start.source_code'))}",
-                    "https://github.com/aykhan026/",
+                    "https://github.com/Hesenovhuseyn/",
                     "url",
                 ),
             ],
