9cc11c980adc6a5c0fd633f112cbd748f9143789
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-03-07 16:33:18 +0400

    update tesadufu_cavab.py

--- Nezrin/komekci/tesadufu_cavab.py
+++ Nezrin/komekci/tesadufu_cavab.py
@@ -2,3 +2,8 @@ import random
 # oğul_ekmehaa #
 async def tes_cavab(fname):
     return random.choice(open(fname).read().splitlines())
+
+import random
+# oğul_ekmehaa #
+async def tes_cavab(fname):
+    return random.choice(open(fname).read().splitlines())
