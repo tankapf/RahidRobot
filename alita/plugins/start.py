ec896cb0b9ce8610666a3742c592614941d42937
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-03-08 01:45:06 +0400

    update start.py

--- alita/plugins/start.py
+++ alita/plugins/start.py
@@ -54,6 +54,7 @@ async def start(bot:Alita Client, message: Message):
     await bot.send_photo(photo='https://telegra.ph/file/5ee3fb631295ef692635d.jpg")
     
 async def start(c: Alita, m: Message):
+    c.send
     if m.chat.type == "private":
         if len(m.text.split()) > 1:
             help_option = (m.text.split(None, 1)[1]).lower()
