3f965a2f620d6423f806be67b70e626597220826
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-03-08 01:43:33 +0400

    update start.py

--- alita/plugins/start.py
+++ alita/plugins/start.py
@@ -50,7 +50,7 @@ async def close_admin_callback(_, q: CallbackQuery):
 @Alita.on_message(
     command("start") & (filters.group | filters.private),
 )
-async def start(bot: Client, message: Message):
+async def start(bot:Alita Client, message: Message):
     await bot.send_photo(photo='https://telegra.ph/file/5ee3fb631295ef692635d.jpg")
     
 async def start(c: Alita, m: Message):
