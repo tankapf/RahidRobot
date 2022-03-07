f5a06b7e248789c82d645112196dae3e2afd0ca9
Hesenovhuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-03-07 16:38:03 +0400

    update Sozler.py

--- alita/plugins/Sozler.py
+++ alita/plugins/Sozler.py
@@ -5,14 +5,14 @@ from pyrogram import filters
 from cryptography.fernet import Fernet
 from alita.tr_engine import tlang
 from alita.bot_class import Alita as Nezrin
-from Nezrin.komekci import tes_cavab
+from Nezrin.komekci import tes_zarafat
 
 
-@Nezrin.on_message(filters.command("sehid") & ~filters.edited)
+@Nezrin.on_message(filters.command("/zarafat") & ~filters.edited)
 async def commit(_, message):
-    await message.reply_text((await tes_cavab('Nezrin/txt/sehid.txt')))
+    await message.reply_text((await tes_cavab('Nezrin/txt/zarafat.txt')))
 
-__PLUGIN__ = "sehid"
+__PLUGIN__ = "zarafat"
 _DISABLE_CMDS_ = [
-    "sehid",
+    "zarafat",
 ]
